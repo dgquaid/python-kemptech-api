6c63ed33cd4d40d0a2642f2d5bbe209932c2758f
dannyzen <danny.rosen@gmail.com>
2017-01-05 15:56:09 -0500

    spaces can cause errant white space

--- examples/list_virtual_services.py
+++ examples/list_virtual_services.py
@@ -1,10 +1,10 @@
 # -*- coding: UTF-8 -*- 
 from python_kemptech_api import LoadMaster as loadmaster 
 
-loadmaster_ip = " " # Your LoadMaster administrative IP 
+loadmaster_ip = "" # Your LoadMaster administrative IP 
 # Note: To improve security, avoid using plaintext login and passwords and consider using environmental variables instead.  
-loadmaster_login = " " # Your LoadMaster user login
-loadmaster_password = " " # Your LoadMaster user password 
+loadmaster_login = "" # Your LoadMaster user login
+loadmaster_password = "" # Your LoadMaster user password 
 loadmaster_port = "443" # By default this is 443.  
 lm = loadmaster(loadmaster_ip, loadmaster_login, loadmaster_password, loadmaster_port)  
 
